c6fd34f853210d248bb0eca7231ce80fefce3cfc
Adinan Batista Alves <adinan0803@gmail.com>
2023-06-27 11:44:44 -0300

    fix edição OS

--- app/Http/Controllers/OrdemServicoController.php
+++ app/Http/Controllers/OrdemServicoController.php
@@ -41,8 +41,6 @@ class OrdemServicoController extends Controller
      *
      * @queryParam filter[origem] Filtro de Local (base) do item. Example: Leopoldina
      * @queryParam filter[local_servico] Filtro de nome do local de destino dos materiais. Example: Ibirapuera
-     * @queryParam filter[almoxarife_nome] Filtro do Nome do almoxarife responsável. Example: Fulano
-     * @queryParam filter[almoxarife_email] Filtro do e-mail do almoxarife responsável. Example: fulano@mail.com
      * @queryParam filter[servico_depois_de] Filtro inicial de período da data de serviço. Example: 2023-01-01
      * @queryParam filter[servico_antes_de] Filtro final de período da data de serviço. Example: 2023-12-31
      * @queryParam sort Campo a ser ordenado (padrão ascendente, inserir um hífen antes para decrescente). Colunas possíveis: 'id', 'data_inicio_servico', 'data_fim_servico', 'origem.nome', 'locais.nome' Example: -locais.nome
@@ -61,7 +59,6 @@ class OrdemServicoController extends Controller
         ->where('ordem_servicos.ativo','=',1)
         ->allowedFilters([
                 AllowedFilter::partial('origem','origem.nome'), AllowedFilter::partial('local_servico','locais.nome'),
-                'almoxarife_nome', 'almoxarife_email',
                 AllowedFilter::scope('servico_depois_de'),
                 AllowedFilter::scope('servico_antes_de'),
             ])
@@ -92,11 +89,7 @@ class OrdemServicoController extends Controller
      * @bodyParam status enum required ('A iniciar','Iniciada','Finalizada') Tipo. Example: Iniciada
      * @bodyParam data_inicio_servico datetime required Data do serviço. Example: 2022-08-30T14:48
      * @bodyParam data_fim_servico datetime required Data do serviço. Example: 2022-08-31T17:50
-     * @bodyParam almoxarife_nome string required Nome do Almoxarife. Example: João
-     * @bodyParam almoxarife_email string required E-mail do Almoxarife. Example: joao@teste.com.br
      * @bodyParam especificacao text nullable Especificação. Example: reforma
-     * @bodyParam profissional string nullable Profissional. Example: José
-     * @bodyParam horas_execucao integer nullable Horas de execução. Example: 10
      * @bodyParam observacoes text nullable Observações. Example: observações referente ao serviço
      * @bodyParam user_id integer required ID do usuário. Example: 1
      *
@@ -110,11 +103,7 @@ class OrdemServicoController extends Controller
      *         "status": "Iniciada",
      *         "data_inicio_servico": "2022-08-30T14:48",
      *         "data_fim_servico": "2022-08-31T17:50",
-     *         "almoxarife_nome": "João",
-     *         "almoxarife_email": "joao@teste.com.br",
      *         "especificacao": "reforma",
-     *         "profissional": "José",
-     *         "horas_execucao": 10,
      *         "observacoes": "observações referente ao serviço",,
      *         "flg_baixa": 0,
      *         "user_id": 1,
@@ -153,20 +142,16 @@ class OrdemServicoController extends Controller
         $ordem_servico->status = $request->input('status');
         $ordem_servico->data_inicio_servico = HtmlHelper::converteDatetimeLocal2MySQL($request->input('data_inicio_servico'));
         $ordem_servico->data_fim_servico = HtmlHelper::converteDatetimeLocal2MySQL($request->input('data_fim_servico'));
-        //$ordem_servico->almoxarife_nome = $request->input('almoxarife_nome');
-        //$ordem_servico->almoxarife_email = $request->input('almoxarife_email');
         $ordem_servico->especificacao = $request->input('especificacao');
-        //$ordem_servico->profissional = $request->input('profissional');
-        //$ordem_servico->horas_execucao = $request->input('horas_execucao');
         $ordem_servico->observacoes = $request->input('observacoes');
         $ordem_servico->user_id = Auth::user()->id;
-        
+
         $ordem_servico->numero_ordem_servico = $request->input('numero_ordem_servico');
-        
+
         if (is_null($ordem_servico->numero_ordem_servico)){
             $this->validate($request, [
                 'justificativa_os' => 'required'
-            ]); 
+            ]);
 
             $ordem_servico->justificativa_os = $request->input('justificativa_os');
         }
@@ -265,11 +250,7 @@ class OrdemServicoController extends Controller
      *         "status": "Iniciada",
      *         "data_inicio_servico": "2022-08-30T14:48",
      *         "data_fim_servico": "2022-08-31T17:50",
-     *         "almoxarife_nome": "João",
-     *         "almoxarife_email": "joao@teste.com.br",
      *         "especificacao": "reforma",
-     *         "profissional": "José",
-     *         "horas_execucao": 10,
      *         "observacoes": "observações referente ao serviço",
      *         "flg_baixa": 0,
      *         "user_id": 1
@@ -306,11 +287,7 @@ class OrdemServicoController extends Controller
      * @bodyParam status enum required ('A iniciar','Iniciada','Finalizada') Tipo. Example: Iniciada
      * @bodyParam data_inicio_servico datetime required Data do serviço. Example: 2022-08-30T14:48
      * @bodyParam data_fim_servico datetime required Data do serviço. Example: 2022-08-31T17:50
-     * @bodyParam almoxarife_nome string required Nome do Almoxarife. Example: João
-     * @bodyParam almoxarife_email string required E-mail do Almoxarife. Example: joao@teste.com.br
      * @bodyParam especificacao text nullable Especificação. Example: reforma
-     * @bodyParam profissional string nullable Profissional. Example: José
-     * @bodyParam horas_execucao integer nullable Horas de execução. Example: 10
      * @bodyParam observacoes text nullable Observações. Example: observações referente ao serviço
      * @bodyParam user_id integer required ID do usuário. Example: 1
      *
@@ -324,11 +301,7 @@ class OrdemServicoController extends Controller
      *         "status": "Iniciada",
      *         "data_inicio_servico": "2022-08-30T14:48",
      *         "data_fim_servico": "2022-08-31T17:50",
-     *         "almoxarife_nome": "João",
-     *         "almoxarife_email": "joao@teste.com.br",
      *         "especificacao": "reforma",
-     *         "profissional": "José",
-     *         "horas_execucao": 10,
      *         "observacoes": "observações referente ao serviço",
      *         "flg_baixa": 0,
      *         "user_id": 1
@@ -344,11 +317,7 @@ class OrdemServicoController extends Controller
         $ordem_servico->status = $request->input('status');
         $ordem_servico->data_inicio_servico = HtmlHelper::converteDatetimeLocal2MySQL($request->input('data_inicio_servico'));
         $ordem_servico->data_fim_servico = HtmlHelper::converteDatetimeLocal2MySQL($request->input('data_fim_servico'));
-        $ordem_servico->almoxarife_nome = $request->input('almoxarife_nome');
-        $ordem_servico->almoxarife_email = $request->input('almoxarife_email');
         $ordem_servico->especificacao = $request->input('especificacao');
-        $ordem_servico->profissional = $request->input('profissional');
-        $ordem_servico->horas_execucao = $request->input('horas_execucao');
         $ordem_servico->observacoes = $request->input('observacoes');
         $ordem_servico->user_id = Auth::user()->id;
 
@@ -436,11 +405,7 @@ class OrdemServicoController extends Controller
      *         "status": "Iniciada",
      *         "data_inicio_servico": "2022-08-30T14:48",
      *         "data_fim_servico": "2022-08-31T17:50",
-     *         "almoxarife_nome": "João",
-     *         "almoxarife_email": "joao@teste.com.br",
      *         "especificacao": "reforma",
-     *         "profissional": "José",
-     *         "horas_execucao": 10,
      *         "observacoes": "observações referente ao serviço",
      *         "flg_baixa": 1,
      *         "user_id": 1
@@ -531,11 +496,7 @@ class OrdemServicoController extends Controller
      *         "status": "Iniciada",
      *         "data_inicio_servico": "2022-08-30T14:48",
      *         "data_fim_servico": "2022-08-31T17:50",
-     *         "almoxarife_nome": "João",
-     *         "almoxarife_email": "joao@teste.com.br",
      *         "especificacao": "reforma",
-     *         "profissional": "José",
-     *         "horas_execucao": 10,
      *         "observacoes": "observações referente ao serviço",
      *         "user_id": 1
      *     }
