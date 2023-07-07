bae8d83855d41808ca859227425d02c88279c6bb
Adinan Batista Alves <adinan0803@gmail.com>
2023-07-07 10:24:24 -0300

    alterar campos de baixa para null

---
+++ database/migrations/2023_07_07_124818_alterar_nulls.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterarNulls extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('saidas', function (Blueprint $table) {
+            //
+            $table->unsignedBigInteger('baixa_user_id')->nullable()->change();
+            $table->dateTime('baixa_datahora')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('saidas', function (Blueprint $table) {
+            //
+        });
+    }
+}
