12f348e6447b1625282549f5f5146c5a97d082ff
Vinicius Casotti de Campos Oliveira <viniciusccoliveira@PREFEITURA.SP.GOV.BR>
2024-06-14 14:32:47 -0300

    alteração no resource de transferencia

--- app/Http/Requests/TipoServicoFormRequest.php
+++ app/Http/Requests/TipoServicoFormRequest.php
@@ -15,7 +15,7 @@ class TipoServicoFormRequest extends FormRequest
     {
         return true;
     }
-
+    
     /**
      * Get the validation rules that apply to the request.
      *
--- app/Http/Resources/TransferenciaDeMateriais.php
+++ app/Http/Resources/TransferenciaDeMateriais.php
@@ -18,12 +18,12 @@ class TransferenciaDeMateriais extends JsonResource
             'id' => $this->id,
             'destino' => $this->base_destino ? $this->base_destino->nome : null,
             'origem' => $this->base_origem ? $this->base_origem->nome : null,
+            'origem_json' => $this->base_origem ? $this->base_origem : null,
             'base_destino_id' => $this->base_destino_id,
             'base_origem_id' => $this->base_origem_id,
             'data_transferencia' => $this->data_transferencia,
             'status' => $this->status,
             'observacao' => $this->observacao,
-            'observacao_motivo' => $this->observacao_motivo,
             'observacao_motivo' => $this->observacao_motivo,
             'observacao_user_id' => $this->observacao_user_id,
         ];
